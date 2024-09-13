bce315473b77b469e711bc55ebde9046756c51bb
Nabil L.A. <nabil14716@gmail.com>
2024-09-13 12:23:50 +0100

    doc: Se aniaden apuntes basicos en las practicas de casa

--- repaso/home-practice/scripts/home-practice01.js
+++ repaso/home-practice/scripts/home-practice01.js
@@ -1,3 +1,34 @@
-// control structrures
 
-let 
+
+/**
+ * Array & objects practice
+ */
+let name = "Dion";
+let series = "FFXVI";
+let age = 29;
+
+let character = {
+    name: "Dion",
+    series : "FFXVI",
+    age : 29, 
+}
+
+console.log(character);
+console.log(character.name);
+console.log(character['series']);
+
+let videogames = ['FF', 'DMC'];
+console.log(videogames);
+console.log(videogames[0]);
+
+videogames[2]= 'FE';
+
+/**
+ * Functions
+ */
+
+function greetings(){
+    console.log('Hello World 2!')
+}
+
+greetings();
