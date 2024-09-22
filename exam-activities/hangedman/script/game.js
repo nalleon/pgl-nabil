cb903c249a6cb1d62f11d728135d952b9cec0b65
Nabil L.A. <nabil14716@gmail.com>
2024-09-22 12:30:37 +0100

    feat(hangedman): Commenced js

--- exam-activities/hangedman/script/game.js
+++ exam-activities/hangedman/script/game.js
@@ -1,4 +1,19 @@
+const DOM = {
+    playBtn : document.getElementById('play'),
+    letterChosen : document.getElementById('letterChosen'),
+    words : ["esternocleidomastoideo", "paralelepípedo", "otorrinolaringología", "electrocardiograma", "ornitorrinco"]
+}
 
-Game {
 
+function startGame(event) {
+    console.log('Play Button Clicked');
+    rndWord(0, DOM.words.length);
+}
+
+DOM.playBtn.addEventListener('click', startGame);
+
+function rndWord(minNum, maxNum) {
+    let valueRanges = maxNum - minNum;
+    let rndValue = Math.trunc(Math.random() * valueRanges)+minNum; ¡
+    console.log(rndValue);
 }
