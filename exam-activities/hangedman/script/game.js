141a4450c4a8e5f2b9469b94f3456f7883ae1582
Nabil L.A. <nabil14716@gmail.com>
2024-09-22 15:18:40 +0100

    docs(hangedman): Documented functions of the scripts

--- exam-activities/hangedman/script/game.js
+++ exam-activities/hangedman/script/game.js
@@ -9,31 +9,46 @@ const DOM = {
 
 let globalWordChosen = "";
 
+/**
+ * Function to start the game when the play button is clicked.
+ * @param {*} event 
+ */
 function startGame(event) {
     console.log('Play Button Clicked');
     let wordPos = rndWord(0, DOM.words.length);
-    _word(wordPos);
+    showWord(wordPos);
 }
 
 DOM.playBtn.addEventListener('click', startGame);
 
+
+/**
+ * Function to select a random word from the array
+ * @param {*} minNum array min number of words
+ * @param {*} maxNum array max number of words
+ * @returns random word position in the array
+ */
 function rndWord(minNum, maxNum) {
     let valueRanges = maxNum - minNum;
     let rndValue = Math.trunc(Math.random() * valueRanges)+minNum;
-    console.log(rndValue);
-
     return rndValue;
 }
 
-function _word(wordPos){
+/**
+ * Function to select the word and show it in the html
+ * @param {*} wordPos position of the word in the array
+ */
+
+function showWord(wordPos){
     let wordChosen = DOM.words[wordPos];
-    globalWordChosen = wordChosen
+    globalWordChosen = wordChosen;
+
     let wordLength = wordChosen.length;
 
-    console.log(wordChosen);
+  //  console.log(wordChosen);
    
   // const wordToGuess = wordChosen.replace(/[a-zA-Z]/g, '_'); // /g --> global, applaies to all regular expressions
-  // console.log(wordToGuess);
+  // console.log(wordToGuess); 
     
     for (let i = 0; i < wordLength; i++) {
         let letter = document.createElement('span');
@@ -44,6 +59,10 @@ function _word(wordPos){
 
 
 }
+
+/**
+ * Event listener for when the user types a letter and hits enter.
+ */
 
 DOM.letterChosen.addEventListener('keyup', (event) => {
     if (event.key == 'Enter') {
@@ -61,7 +80,10 @@ DOM.letterChosen.addEventListener('keyup', (event) => {
     }
 }); 
 
-
+/**
+ * Function to update the word in the HTML based on the correct letters entered by the user.
+ * @param {*} userLetter 
+ */
 function correctLetter(userLetter) {
     let correctIndexes = [];
     const globalWordChosenArray = globalWordChosen.split('');
@@ -75,10 +97,20 @@ function correctLetter(userLetter) {
     drawWord(userLetter, correctIndexes);
 }
 
+/**
+ * Fuction to update the used letters in the HTML.
+ * @param {*} letter 
+ */
+
 function usedLetters(letter) {
     DOM.usedLetters.innerText += letter + ',  ';
 }
 
+/**
+ * Function to update the word in the HTML based on the correct letters entered by the user.
+ * @param {*} letter the letter entered by the user
+ * @param {*} indexes index of the letter appaerences to update
+ */
 function drawWord(letter, indexes){
     console.log(DOM.wordToGuess.getElementsByTagName('span'));
     indexes.forEach(element => {
@@ -89,3 +121,4 @@ function drawWord(letter, indexes){
         })
     });
 }
+
