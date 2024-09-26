fe3f2f3adba28d372b93786a131964a9378854bf
Nabil L.A. <nabil14716@gmail.com>
2024-09-26 11:51:53 +0100

    feat(practice06): Added terminal param

--- unit-01/node-dossier/practices/practice06/app.js
+++ unit-01/node-dossier/practices/practice06/app.js
@@ -7,7 +7,20 @@ En el interior del método escribir() se llama a la función: fs.writeFileSync()
 const {write} = require("./utils/fileManagement");
 const {createTable} = require("./model/table");
 
-write("practice06.txt", createTable(8))
+
+function writeTableParam(){
+    
+    let num = parseInt(process.argv[2]);
+    
+    if(!Number.isInteger(num) || num <= 0){
+        console.log("Error: You must use an integer and positive number.");
+        process.exit(1);
+    }
+
+    write("practice06.txt", createTable(num))
     .then(console.log("save ok"))
     .catch(err => console.log("error"));
-    
+}
+
+
+writeTableParam();
