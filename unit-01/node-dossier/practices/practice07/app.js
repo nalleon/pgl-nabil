224d3310482235b0f54487a257deef7cfe401029
nalleon <nabil14716@gmail.com>
2024-09-26 15:11:43 +0100

    feat(practice07): Finished practice07

--- unit-01/node-dossier/practices/practice07/app.js
+++ unit-01/node-dossier/practices/practice07/app.js
@@ -6,24 +6,16 @@ En el interior del método escribir() se llama a la función: fs.writeFileSync()
 
 const {write} = require("./utils/fileManagement");
 const {createTable} = require("./model/table");
-const tyrgs = require("yargs");
 
+const yargs = require("yargs/yargs");
+const { hideBin } = require('yargs/helpers')
+const argv = yargs(hideBin(process.argv)).argv
 
-
-
-yargs.command("writeTable", "Write a table in a file", {}, writeTableParam)
+console.log(argv);
 
 
 function writeTable(){
-    
-    let num = parseInt(process.argv[2]);
-    
-    if(!Number.isInteger(num) || num <= 0){
-        console.log("Error: You must use an integer and positive number.");
-        process.exit(1);
-    }
-
-    write("practice07.txt", createTable(num))
+    write("practice07.txt", createTable(argv.table))
     .then(console.log("save ok"))
     .catch(err => console.log("error"));
 }
