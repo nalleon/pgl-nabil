c2e288bc7e0a6df1fbef41827a62bff57a1787e5
Nabil L.A. <nabil14716@gmail.com>
2024-10-31 12:52:59 +0000

    feat(dossier-react): Finished practice34

---
+++ unit-01/js-app/src/practice34/Practica34.tsx
@@ -0,0 +1,52 @@
+
+
+import React, { useState } from 'react'
+
+
+type Props = {}
+
+
+const Practice34 = (props: Props) => {
+    const [age, setAge] = useState<string[]>([]);
+
+    function addDogAge(e:React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        let form = e.currentTarget;
+        let age = form.age.value;
+        let sizeDog = form.sizeDog.value;
+
+        let ageUpdate = 0;
+
+        if (sizeDog == "Small") {
+            ageUpdate = age * 20;
+        } else if (sizeDog == "Medium") {
+            ageUpdate = age * 18;
+        } else if (sizeDog == "Big") {
+            ageUpdate = age * 16;
+        }
+        let mensaje : string = " dog's age: " + age + ", human age: " + ageUpdate;
+        setAge([...age, mensaje]);
+    }
+
+return (
+    <>
+    <p>{age}</p>
+    <h2>Add dog's age</h2>
+    <form onSubmit={addDogAge}>
+        <input type="number" name="age" id="age" placeholder="Write your dog's age"/>
+        <input type="radio" name="sizeDog" id="smallDog" value="Small"/>
+        <label htmlFor="smallDog">Small</label>
+        <input type="radio" name="sizeDog" id="mediumDog" value="Medium"/>
+        <label htmlFor="mediumDog">Medium</label>
+        <input type="radio" name="sizeDog" id="tipogrande" value="Big"/>
+        <label htmlFor="bigDog">Big</label>
+        <input type="submit" value="Add"/>
+    </form>
+    </>
+)
+}
+
+
+export default Practice34
+
+
