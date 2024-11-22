0a5e4f0b3352186bc1f19650f0d3f2f32f5f3f09
Nabil L.A. <nabil14716@gmail.com>
2024-11-22 03:27:39 +0000

    feat(dossier-node): Finished markdown

--- unit-01/dossier-node/practices/practice05.js
+++
@@ -1,5 +0,0 @@
-/**
- * Crear la aplicación descrita. El fichero: manejarTabla.js debe exportar un
-método: escribir(nombreDelFichero, textoEscribir ) que tiene que devolver una promesa.
-En el interior del método escribir() se llama a la función: fs.writeFileSync()
- */
