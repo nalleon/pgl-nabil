08075456a24d284c2e5ecab0908a616192a4acdb
Nabil L.A. <nabil14716@gmail.com>
2024-11-28 12:04:41 +0000

    feat(reactnative): Finished practice01

---
+++ unit-02/ReactNativeDossier/practices/practice01/components/Practice01.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import styles from '../styles/Practice01Styles';
+type Props = {}
+
+const Practice01 = (props: Props) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>¡Hola mundo! Soy Nabil</Text>
+    </View>
+  )
+}
+
+export default Practice01
+
+
