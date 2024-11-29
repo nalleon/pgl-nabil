4671f5482ba48241d91589956f0a34b145bba6e7
Nabil L.A. <nabil14716@gmail.com>
2024-11-29 12:43:57 +0000

    feat(movies-project): Added styles and links to navbar

--- exam-activities/movies-project/src/Components/MoviesApp.tsx
+++ exam-activities/movies-project/src/Components/MoviesApp.tsx
@@ -8,6 +8,9 @@ import DeleteMovie from './DeleteMovie';
 import FindMovie from './FindMovie';
 import Login from './Login';
 import { BrowserRouter, Route, Link, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 
 type Props = {}
@@ -15,32 +18,41 @@ type Props = {}
 const MoviesApp = (props: Props) => {
     return (
         <BrowserRouter>
-        <AppContextProvider>
-            <h1>App</h1>
-            <Navbar />
-            <Routes>
-                <Route path="/" element={<Login />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/movies" element={<MovieList/>}/>
-                <Route path="/movies/find-movie" element={<FindMovie/>}/>
-                <Route path="/movies/movie/:movieId" element={<MovieDetails/>}/>
-                <Route path="/movies/create-movie" element={<CreateMovie/>}/>
-                <Route path="/movies/modify-movie" element={<UpdateMovie/>}/>
-                <Route path="/movies/delete-movie" element={<DeleteMovie/>}/>
-            </Routes>
-        </AppContextProvider>
-    </BrowserRouter>
+            <AppContextProvider>
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<Login />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/movies" element={<MovieList/>}/>
+                    <Route path="/movies/find-movie" element={<FindMovie/>}/>
+                    <Route path="/movies/movie/:movieId" element={<MovieDetails/>}/>
+                    <Route path="/movies/create-movie" element={<CreateMovie/>}/>
+                    <Route path="/movies/modify-movie" element={<UpdateMovie/>}/>
+                    <Route path="/movies/delete-movie" element={<DeleteMovie/>}/>
+                </Routes>
+            </AppContextProvider>
+        </BrowserRouter>
     )
 
     function Navbar() {
         return (
-            <nav>
-                <Link to="/login">Login </Link>
-                <Link to="/movies"> Movies </Link>
-                <Link to="/movies/find-movie"> Find movie </Link>
-                <Link to="/movies/create-movie"> Create </Link>
-                <Link to="/movies/modify-movie"> Modify </Link>
-                <Link to="/movies/delete-movie"> Delete </Link>
+            <nav className='d-flex justify-content-center bg-dark navbar navbar-dark text-light navbar-expand-l'>
+                <div className="container-fluid">
+                    <h3 className="mt-2 me-2 ms-2"><Link to="/movies" className='link-light link-offset-2 link-underline link-underline-opacity-0 m-3'><i className="ms-3 bi bi-film text-light me-2"></i>Movies </Link></h3>
+                    <button className="navbar-toggler bg-dark" type="button" data-bs-toggle="collapse" data-bs-target="#navBar" aria-controls="navBar" aria-expanded="true" aria-label="Toggle navigation" >
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                        <div id="navBar" className="collapse navbar-collapse">
+                            <ul className="d-flex align-items-start navbar-nav me-auto mb-2 mb-lg-0 ms-5">
+                                <li className="list-group-item m-1"><Link to="/movies" className='link-secondary link-offset-2 link-underline link-underline-opacity-0 m-3 '> Movies </Link></li>
+                                <li className="list-group-item m-1"><Link to="/movies/find-movie" className='link-secondary link-offset-2 link-underline link-underline-opacity-0 m-3'> Find movie </Link></li>
+                                <li className="list-group-item m-1"><Link to="/movies/create-movie" className='link-secondary link-offset-2 link-underline link-underline-opacity-0 m-3'> Create </Link></li>
+                                <li className="list-group-item m-1"><Link to="/movies/modify-movie" className='link-secondary link-offset-2 link-underline link-underline-opacity-0 m-3'> Modify </Link></li>                           
+                                <li className="list-group-item m-1">  <Link to="/movies/delete-movie" className='link-secondary link-offset-2 link-underline link-underline-opacity-0 m-3'> Delete </Link></li>                            
+                                <li className="list-group-item m-1"><Link to="/login" className='link-secondary link-light-hover link-offset-2 link-underline link-underline-opacity-0 m-3' >Login </Link></li>                            
+                            </ul>
+                        </div>
+                </div>
             </nav>
         );
     }
