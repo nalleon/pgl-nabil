5eb79c4aaae5fe9344a1191b607871f2d6e15bf4
Nabil L.A. <nabil14716@gmail.com>
2024-12-07 13:31:55 +0000

    feat(movies-project): Added categories to json

---
+++ exam-activities/movies-project/src/Components/MoviesPerCategory.tsx
@@ -0,0 +1,45 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+
+type Props = {}
+
+type CategoryType = {
+    id: number;
+    name: string;
+    descriptoin: string;
+}
+const MoviesPerCategory = (props: Props) => {
+    const [categories, setCategories] = useState<CategoryType[]>([]);
+
+    const uri = 'http://localhost:3000/categories';
+
+    useEffect(() => {
+        fetchCategories();
+    }, [])
+    
+    /**
+     * Function to fetch categories from the api
+     */
+    const fetchCategories = async () => {
+        try {
+            const response = await axios.get(uri);
+            setCategories(response.data);
+        } catch (error) {
+            console.error('Error fetching categories:', error);
+        }
+    }
+
+        
+    return (
+        <>
+            <div className="container">
+                <h3>CATEGORIES</h3>
+                <div className="row">
+                    
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default MoviesPerCategory
