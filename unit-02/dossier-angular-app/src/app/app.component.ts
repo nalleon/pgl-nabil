9d2852599b879f3fd1bae9b6912bd41d589aca7b
nalleon <nabil14716@gmail.com>
2025-01-27 18:42:00 +0000

    feat(angular): Create angular dossier and finished practice 1 y 2

---
+++ unit-02/dossier-angular-app/src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  nombre : string ='Nabil';
+  apellido : string ='L. A.';
+  title = 'dossier-angular-app';
+  numArr = [1,2,3,4,5,6,7,8,9,10];
+  result : number[]= [];
+  tableResult : string[] = [];
+
+
+  ejecutar(num : number){
+    let arrTable : number[] = []
+    for(let i=0; i< this.numArr.length; i++){
+      arrTable[i] = num*this.numArr[i];
+      this.tableResult[i] = '\n ' + num + ' * ' +  this.numArr[i] + ' = '+ arrTable[i];
+    }
+    this.result = arrTable;
+
+    //alert(this.table);
+
+
+  }
+}
