3cd25e71e0187dd43145e9ccfb17f6f0cffa7987
nalleon <nabil14716@gmail.com>
2025-01-28 13:14:31 +0000

    feat(instituto-app): Added userprofile screen to drawer

--- exam-activities/instituto-app/src/navigation/DrawerNav.tsx
+++ exam-activities/instituto-app/src/navigation/DrawerNav.tsx
@@ -1,15 +1,15 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import AlumnoProfile from '../screens/AlumnoProfile';
 import TabNav from './TabNav';
+import UserProfile from '../screens/UserProfile';
 
 type Props = {}
 const Drawer = createDrawerNavigator();
 const DrawerNav = (props: Props) => {
   return (
     <Drawer.Navigator id={undefined}>
-      <Drawer.Screen name="Perfil" component={AlumnoProfile} options={{ title: 'Mi perfil' }} />
+      <Drawer.Screen name="Perfil" component={UserProfile} options={{ title: 'Mi perfil' }} />
       <Drawer.Screen name="Operaciones" component={TabNav} options={{ title: 'Operaciones' }}/>
     </Drawer.Navigator>
   )
---
+++ exam-activities/instituto-app/src/screens/UserProfile.tsx
@@ -0,0 +1,54 @@
+import { StyleSheet, Text, View, Image, ScrollView } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import axios, { spread } from 'axios';
+import { FlatList } from 'react-native-gesture-handler';
+
+import Icon  from 'react-native-vector-icons/Ionicons';
+
+type Props = {}
+
+type UserData = {
+    name : string,
+    email : string
+}
+const UserProfile = (props: Props) => {
+    /**
+     * UseStates
+     */
+    const [data, setData] = useState<UserData>({} as UserData);
+    const uri = ""
+
+    //TODO: add database search 
+
+    useEffect(() => {
+        /**const fetchData = async () => {
+            const response = await axios.get(uri);
+            let userData = response.data as UserData;
+            setData(userData);
+          }
+
+        fetchData();
+        */
+       setData({
+        name: "example",
+        email: "example@email.com"
+       });
+    }, [data])
+    
+
+  return (
+    <View>
+        
+        <Text>
+            { data.name }
+            ---
+            { data.email}
+        </Text>
+    </View>
+  )
+}
+
+export default UserProfile
+
+const styles = StyleSheet.create({})
