6904c55ee2bc7549578ad87b2db42e067fd847a2
nalleon <nabil14716@gmail.com>
2025-01-28 13:01:12 +0000

    feat(instituto-app): Added stack navigation

--- exam-activities/instituto-app/src/navigation/AlumnoStackNav.tsx
+++ exam-activities/instituto-app/src/navigation/AlumnoStackNav.tsx
@@ -1,13 +1,55 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import FindAlumno from '../screens/FindAlumno';
+import AlumnoProfile from '../screens/AlumnoProfile';
 
 type Props = {}
 
+type AlumnoDetails = {
+  FindAlumno: undefined,
+  AlumnoProfile: {dni : string},
+}
+
+const Stack = createNativeStackNavigator<AlumnoDetails>();
+
 const AlumnoStackNav = (props: Props) => {
   return (
-    <View>
-      <Text>AlumnoStackNav</Text>
-    </View>
+    <Stack.Navigator id={undefined}
+    screenOptions={{contentStyle:{backgroundColor:'white'}}}
+>
+    <Stack.Screen 
+        name="FindAlumno" 
+        component={FindAlumno} 
+        options={{ title: 'Buscador',  headerTitleAlign: 'center', 
+            headerStyle: {
+                backgroundColor: '#008080', 
+            },
+            headerTintColor: 'white', 
+            headerTitleStyle: {
+                fontWeight: 'bold',    
+                fontSize: 20,                
+            }
+        }}
+    />
+    
+    <Stack.Screen 
+        name="AlumnoProfile" 
+        component={AlumnoProfile} 
+        options={({ route }) => ({
+            title: `${route.params.dni.toUpperCase()}`,
+            headerTitleAlign: 'center',
+            headerStyle: {
+                backgroundColor: '#008080', 
+            },
+            headerTintColor: 'white', 
+            headerTitleStyle: {
+                fontWeight: 'bold',    
+                fontSize: 20,      
+            }
+        })} 
+    />
+</Stack.Navigator>
   )
 }
 
--- exam-activities/instituto-app/src/navigation/TabNav.tsx
+++ exam-activities/instituto-app/src/navigation/TabNav.tsx
@@ -1,13 +1,48 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, useWindowDimensions, View } from 'react-native'
 import React from 'react'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/Ionicons';
+import AlumnoProfile from '../screens/AlumnoProfile';
+import AlumnoStackNav from './AlumnoStackNav';
+import AddAlumnoScreen from '../screens/AddAlumnoScreen';
+import DeleteAlumnoScreen from '../screens/DeleteAlumnoScreen';
 
 type Props = {}
+const Tab = createBottomTabNavigator();
 
 const TabNav = (props: Props) => {
+  const { width, height} = useWindowDimensions();
+  const isHorizontal = width > height;
   return (
-    <View>
-      <Text>TabNav</Text>
-    </View>
+      <Tab.Navigator id={undefined}
+          screenOptions={{
+              headerShown:false,
+              tabBarShowLabel: false,
+              tabBarPosition: isHorizontal ? 'left' : 'bottom',
+              tabBarVariant: isHorizontal ? 'material' : 'uikit',
+              tabBarLabelPosition: 'below-icon',
+          }}
+          >   
+          
+          <Tab.Screen name='Agregar alumno' component={AddAlumnoScreen}
+              options={ {tabBarIcon: ({focused}) => 
+                  <Icon name={(focused) ? 'person-add' : 'person-add-outline'} size={30}/>
+              }
+          }/>
+
+          <Tab.Screen name='Eliminar alumno' component={DeleteAlumnoScreen}
+              options={ {tabBarIcon: ({focused}) => 
+                  <Icon name={(focused) ? 'trash' : 'trash-outline'} size={30}/>
+              }
+          }/>
+
+          <Tab.Screen name='Buscar' component={AlumnoStackNav}
+              options={ {tabBarIcon: ({focused}) => 
+                  <Icon name={(focused) ? 'search' : 'search-outline'} size={30}/>
+              }
+          }/>
+              
+      </Tab.Navigator>
   )
 }
 
