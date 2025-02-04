690be9879b34d74eadcad5dd6dd609c139b020a4
nalleon <nabil14716@gmail.com>
2025-02-04 23:32:45 +0000

    refactor(dto): Correced enrollment entity

--- exam-activities/instituto-app/src/data/entities/Matricula.ts
+++ exam-activities/instituto-app/src/data/entities/Matricula.ts
@@ -1,20 +1,32 @@
-import { BaseEntity, Column, Entity, ManyToMany, ManyToOne, OneToMany, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
+import { BaseEntity, Column, Entity, JoinTable, ManyToMany, ManyToOne, OneToMany, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
 import { Alumno } from "./Alumno";
 import { Asignatura } from "./Asignatura";
 
-@Entity('Matricula')
+@Entity('matriculas')
 export class Matricula extends BaseEntity
 {
     @PrimaryGeneratedColumn()
     id: number;
 
-    @Column('int')
+    @Column('integer')
     year: number;
-    
+
     @ManyToOne(() => Alumno, (alumno) => alumno.matriculas)
     alumno: Alumno;
 
     @ManyToMany(() => Asignatura, (asignatura) => asignatura.matriculas)
+    @JoinTable(
+    {
+      name: 'asignaturas_matriculas',
+      joinColumn: { 
+        name: 'idmatricula',
+        referencedColumnName: 'id'
+      },
+      inverseJoinColumn: { 
+        name: 'idasignatura',
+        referencedColumnName: 'id',
+      }
+    })
     asignaturas: Asignatura[];
 
 }
